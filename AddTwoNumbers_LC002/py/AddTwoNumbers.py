3e2471e3deda9e805492d3807cab800b5ceb50c3
jinghaoxu <jinghaoxu2007@gmail.com>
2019-01-14 14:52:17 -0800

    Updated AddTwoNumbers with new LinkedList class or just ListNode class

--- AddTwoNumbers_LC002/py/AddTwoNumbers.py
+++ AddTwoNumbers_LC002/py/AddTwoNumbers.py
@@ -3,6 +3,55 @@ class ListNode(object):
     def __init__(self, x):
         self.val = x
         self.next = None
+    
+    def __repr__(self):
+        if not self:
+            return 'Null'
+        else:
+            # here call repr(ListNode)
+            return "{}->{}".format(self.val, repr(self.next))
+
+class LinkedList(object):
+    """
+    Create a new class of LinkedList is ok
+    But it doesn't meet the LeetCode's requirements: where the rtype of addTwoNumbers() needs to be a ListNode, not LinkedList
+    And Leet Code can't know we have a new class LinkedList defined
+    """
+    def __init__(self):
+        # create a class's variable, so it will be shared by all member functions
+        # we don't need to return it in each member function
+        self.head = None
+    
+    def insertNodeToHead(self, val):
+        newNode = ListNode(val)
+        newNode.next = self.head
+        self.head = newNode
+    
+    def insertNodeToTail(self, val):
+        p = self.head
+        newNode = ListNode(val)
+        if p is None:
+            self.head = newNode
+        else:
+            while p.next:
+                p = p.next
+        
+            p.next = newNode 
+    
+    def printLinkedList(self):
+        print("\nPrint out linked list: ")
+        p = self.head
+        while p:
+            print("{}->".format(p.val), end='')
+            p = p.next 
+        print("nil")
+
+    def createLinkedListFromNum(self, num):
+        while num:
+            n = num % 10
+            self.insertNodeToTail(n)
+            num = num//10
+        return self.head
 
 
 class Solution(object):
@@ -15,11 +64,47 @@ class Solution(object):
         carry = 0
         newVal = l1.val + l2.val
         if newVal >= 10:
-            newVal = newVal%10
+            newVal %= 10
             carry = 1
         head = ListNode(newVal)
 
-        cur = head 
+        l1 = l1.next
+        l2 = l2.next
+
+        cur = head
+        while l1 or l2:
+            newVal = 0 
+            if l1:
+                newVal += l1.val
+                l1 = l1.next
+            if l2:
+                newVal += l2.val
+                l2 = l2.next
+            newVal += carry
+
+            if newVal >= 10:
+                newVal %= 10
+                carry = 1
+            else:
+                carry = 0
+
+            cur.next = ListNode(newVal)
+            cur = cur.next
+       
+        if carry == 1: 
+            cur.next = ListNode(1)
+
+        return head 
+
+    def addTwoNumbersUsingLinkedListClass(self, l1, l2):
+        newList = LinkedList()
+        carry = 0
+        newVal = l1.val + l2.val
+        if newVal >= 10:
+            newVal -= 10
+            carry = 1
+        newList.insertNodeToTail(newVal)
+
         l1 = l1.next
         l2 = l2.next
 
@@ -30,61 +115,32 @@ class Solution(object):
                 carry = 1
             else:
                 carry = 0
-            cur.next = ListNode(newVal) 
-            cur = cur.next
+            newList.insertNodeToTail(newVal)
             l1 = l1.next
             l2 = l2.next
 
         while l1:
-            cur.next = ListNode(l1.val + carry)
-            cur = cur.next
+            newList.insertNodeToTail(l1.val + carry)
             l1 = l1.next
             carry //= 10
 
         while l2:
-            cur.next = ListNode(l2.val + carry)
-            cur = cur.next
+            newList.insertNodeToTail(l2.val + carry)
             l2 = l2.next
             carry //= 10
         
         if carry == 1: 
-            cur.next = ListNode(1)
+            newList.insertNodeToTail(1)
 
-        return head
-
-    def createLinkedListFromNum(self, num):
-
-        # create head of a linked list using the last digit
-        n = num % 10
-        head = ListNode(n)
-
-        # set up current node
-        cur = head
-
-        # update the number by removing the last digit, e.g. 342 --> 34
-        num = num//10
-
-        # loop the remaining number, append the last digit to the linked list
-        while num:
-            n = num % 10
-            node2 = ListNode(n)
-            cur.next = node2
-            cur = cur.next
-            num = num//10
-
-        return head
-
-    def printLinkedList(self, linkedList):
-        print("\nHere is the linked list content")
-        while linkedList:
-            print("{}->".format(linkedList.val), end='')
-            linkedList = linkedList.next
+        return newList.head 
 
 
 s = Solution()
-linkedList1 = s.createLinkedListFromNum(545)
-s.printLinkedList(linkedList1)
-linkedList2 = s.createLinkedListFromNum(565)
-s.printLinkedList(linkedList2)
-newLinkedList = s.addTwoNumbers(linkedList1, linkedList2)
-s.printLinkedList(newLinkedList)
+lList1 = LinkedList()
+l1 = lList1.createLinkedListFromNum(789)
+lList2 = LinkedList()
+l2 = lList2.createLinkedListFromNum(456)
+print(l1)
+print(l2)
+x = s.addTwoNumbers(l1, l2)
+print(x)
