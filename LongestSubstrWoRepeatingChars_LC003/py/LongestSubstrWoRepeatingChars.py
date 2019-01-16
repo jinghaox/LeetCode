415293b6e4878d7cd9942863e8543bd7ebd9b418
jinghaoxu <jinghaoxu2007@gmail.com>
2019-01-16 15:30:42 -0800

    Update Longest Sbustring Without Repeating characters: print substr

--- LongestSubstrWoRepeatingChars_LC003/py/LongestSubstrWoRepeatingChars.py
+++ LongestSubstrWoRepeatingChars_LC003/py/LongestSubstrWoRepeatingChars.py
@@ -16,16 +16,27 @@ class Solution(object):
         we shall use max(j=3, s[b]+1), which is 3
 
         count the length of the substring by max(i-j+1, res)
+        use m, n to log the index of the longest substring
         """
         strDict = {}
         j = 0
         res = 0
+        m = 0
+        n = 0
         for i in range(len(s)):
+            # if s[i] is already in hashMap, update j 
             if s[i] in strDict:
                 j = max(j, strDict[s[i]]+1)
+            # ow, add s[i] to hashMap
             strDict[s[i]] = i
-            res = max(i-j+1, res)
 
+            # record the length of the longest substr to res
+            if (i-j+1) > res:
+                res = i-j+1
+                m = i
+                n = j
+
+        print(s[n:m+1])
         return res
 
 s = Solution()
